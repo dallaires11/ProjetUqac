52d02130cec5d21f1bdfe12276bc7f5562771ffa
CHROON-PC\Chronos <waterfalldallaire@gmail.com>
2018-10-24 13:39:09 -0400

    Strucutre Usine de traitement

--- TriageDechet/S3_travail2_TriageDechet/GenerateurSeqOperation.h
+++ TriageDechet/S3_travail2_TriageDechet/GenerateurSeqOperation.h
@@ -1,5 +1,4 @@
 #pragma once
-#include"SequenceOperation.h"
 #include"UsineTraitement.h"
 class GenerateurSeqOperation{
 private:
--- TriageDechet/S3_travail2_TriageDechet/UsineTraitement.h
+++ TriageDechet/S3_travail2_TriageDechet/UsineTraitement.h
@@ -1,10 +1,27 @@
 #pragma once
+#include"SequenceOperation.h"
+#include"Depot.h"
+#include"chargementDechet.h"
 class UsineTraitement{
 private:
 	static int instanceUsine;
+	SequenceOperation* sequenceOperation;
+	Depot* depot;
+	CamionBleu* camionBleu;
+	CamionBrun* camionBrun;
+	CamionVert* camionVert;
+	void preOperation();
+	void postOperation();
+	void traiterDTC(Dechet* dechet);
+	void traiterDTNR(Dechet* dechet);
+	void traiterDTR(Dechet* dechet);
+	void traiterDechet(Dechet* dechet);
 public:
 	UsineTraitement();
 	~UsineTraitement();
+
+	void chargerOperation(SequenceOperation* sequenceOperation);
+	void demmarerTraitement(chargementDechet* chargementDechet);//Arranger la majuscule
 
 	static const int getNbInstance() {
 		return UsineTraitement::instanceUsine;
