45f8af4068f2368d25a99143146f8b40f7f1fbcb
stevenduq <44072409+stevenduq@users.noreply.github.com>
2019-10-08 11:25:13 -0400

    Update Dechiffrement.java

--- Chiffrement/src/Dechiffrement/Dechiffrement.java
+++ Chiffrement/src/Dechiffrement/Dechiffrement.java
@@ -1,10 +1,11 @@
 package Dechiffrement;
 
-import java.io.File;
+import java.io.*;
+
 
 public class Dechiffrement {
 
-    public static void main (String [] args){
+    public static void main (String args[] )throws Exception{
         for(int i=0;i< args.length;i++)
         {
             System.out.println(args[i]);
@@ -16,7 +17,14 @@ public class Dechiffrement {
         String cle = args[0];
         String nomFichierOutput = args[1];
         File file = new File(nomFichierOutput);
-
+        System.out.println(file);
+        FileReader fr = new FileReader(file);
+        int i = 0;
+        while((i=fr.read())!=-1)
+            // fonction pour déchifré ici avec chaque caractère (i)
+            System.out.print((char)i);
+        fr.close();
         return;
     }
 }
+
